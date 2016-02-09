606659a4f16ea90797b20b0a6b785d23d0122efe
unhappychoice <unhappychoice@gmail.com>
2016-02-09 22:10:48 +0900

    add CNAME file

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 cd build
+touch CNAME
+echo "blog.unhappychoice.com" >> CNAME
 git init
 git remote add origin https://unhappychoice:${GITHUB_PASSWORD}@github.com/unhappychoice/unhappychoice.github.io.git
 git add .
 git commit -m 'Deployment from CircleCI'
-git push origin master -f
+git push origin master -f >/dev/null 2>&1
