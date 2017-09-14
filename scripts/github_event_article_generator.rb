407a0e4889165b2877352bcebb81ef3b8f78502b
Yuji Ueki <unhappychoice@gmail.com>
2017-09-15 07:32:01 +0900

    :+1: Create 2017-09-01 github activity article automatically

--- scripts/github_event_article_generator.rb
+++ scripts/github_event_article_generator.rb
@@ -15,19 +15,20 @@ require_relative './github_article_generator/generator'
 
 article_date = Time.now.in_time_zone('Asia/Tokyo').beginning_of_month.strftime('%Y-%m-%d')
 article_path = "#{File.expand_path('../../source/articles', __FILE__)}/#{article_date}-github-activity.html.md"
+branch_name = "#{article_date}-github-activity"
 
 `git checkout article_generator`
 `git pull`
-`git checkout -b #{article_date}-github-activity`
+`git checkout -b #{branch_name}`
 
 File.open(article_path, mode = 'w') { |f| f.write(Generator.new.run) }
 
 `git add .`
 `git commit -m ":+1: Create #{article_date} github activity article automatically"`
-`git push`
+`git push --set-upstream origin #{branch_name}`
 
 github = Github.new oauth_token: ENV['GITHUB_TOKEN']
 github.pull_requests.create 'unhappychoice', 'blog',
   title: ":+1: Create #{article_date} github activity article automatically",
-  head: "#{article_date}-github-activity",
+  head: branch_name,
   base: 'master'
