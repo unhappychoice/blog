9a63865117dc68684e0c0973d568b9d0dd81a489
Yuji Ueki <unhappychoice@gmail.com>
2017-09-15 07:19:04 +0900

    :+1: Create pull request function

--- scripts/github_event_article_generator.rb
+++ scripts/github_event_article_generator.rb
@@ -13,9 +13,21 @@ require_relative './github_article_generator/pull_request'
 require_relative './github_article_generator/release'
 require_relative './github_article_generator/generator'
 
-output = Generator.new.run
-
 article_date = Time.now.in_time_zone('Asia/Tokyo').beginning_of_month.strftime('%Y-%m-%d')
 article_path = "#{File.expand_path('../../source/articles', __FILE__)}/#{article_date}-github-activity.html.md"
 
-File.open(article_path, mode = 'w') { |f| f.write(output) }
+`git checkout master`
+`git pull`
+`git checkout -b #{article_date}-github-activity`
+
+File.open(article_path, mode = 'w') { |f| f.write(Generator.new.run) }
+
+`git add .`
+`git commit -m ":+1: Create #{article_date} github activity article automatically"`
+`git push`
+
+github = Github.new oauth_token: ENV['GITHUB_TOKEN']
+github.pull_requests.create 'unhappychoice', 'blog',
+  title: ":+1: Create #{article_date} github activity article automatically",
+  head: "#{article_date}-github-activity",
+  base: 'master'
