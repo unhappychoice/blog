55f9535595532237a2c12dcc9318f7c333e7c63a
Yuji Ueki <unhappychoice@gmail.com>
2017-12-03 05:12:57 +0900

    :bug: Fix ci

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,7 +4,7 @@ cd build
 touch CNAME
 echo "blog.unhappychoice.com" >> CNAME
 git init
-git remote add origin https://unhappychoice:${GITHUB_PASSWORD}@github.com/unhappychoice/unhappychoice.github.io.git
+git remote add origin git@github.com:unhappychoice/unhappychoice.github.io.git
 git add .
 git commit -m 'Deployment from CircleCI'
 git push origin master -f >/dev/null 2>&1
